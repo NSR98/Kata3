32260998d40356932e92eb2182d3aff5ecf98009
Nahuel <>
2018-10-11 09:22:48 +0100

    Kata2 v5

---
+++ src/kata3/Kata3.java
@@ -0,0 +1,9 @@
+package kata3;
+
+public class Kata3 {
+
+    private static HistogramDisplay histo = new HistogramDisplay();
+    public static void main(String[] args) {
+        histo.execute();
+    }
+}
